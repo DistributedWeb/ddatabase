65bde6fc4d9a72df1f99795da0cb11a6ebf895f8
shikhars371 <shikhars371@gmail.com>
2020-09-01 19:21:10 +0530

    var changes

--- test/compat.js
+++ test/compat.js
@@ -1,5 +1,5 @@
 var tape = require('tape')
-var hypercore = require('../')
+var ddatabase = require('../')
 var ram = require('random-access-memory')
 var bufferFrom = require('buffer-from')
 
@@ -27,7 +27,7 @@ tape('deterministic data and tree', function (t) {
 
   function run () {
     var st = storage()
-    var feed = hypercore(st)
+    var feed = ddatabase(st)
 
     feed.append(['a', 'b', 'c', 'd', 'e', 'f'], function () {
       t.same(st.data.toBuffer().toString(), 'abcdef')
@@ -59,11 +59,11 @@ tape('deterministic data and tree after replication', function (t) {
   for (var i = 0; i < 5; i++) run()
 
   function run () {
-    var feed = hypercore(ram)
+    var feed = ddatabase(ram)
 
     feed.append(['a', 'b', 'c', 'd', 'e', 'f'], function () {
       var st = storage()
-      var clone = hypercore(st, feed.key)
+      var clone = ddatabase(st, feed.key)
       var stream = clone.replicate()
 
       stream.pipe(feed.replicate()).pipe(stream).on('end', function () {
@@ -98,7 +98,7 @@ tape('deterministic signatures', function (t) {
 
   function run () {
     var st = storage()
-    var feed = hypercore(st, key, {
+    var feed = ddatabase(st, key, {
       secretKey: secretKey
     })
 
@@ -132,13 +132,13 @@ tape('deterministic signatures after replication', function (t) {
   for (var i = 0; i < 5; i++) run()
 
   function run () {
-    var feed = hypercore(ram, key, {
+    var feed = ddatabase(ram, key, {
       secretKey: secretKey
     })
 
     feed.append(['a', 'b', 'c'], function () {
       var st = storage()
-      var clone = hypercore(st, feed.key)
+      var clone = ddatabase(st, feed.key)
       var stream = clone.replicate()
 
       stream.pipe(feed.replicate()).pipe(stream).on('end', function () {
